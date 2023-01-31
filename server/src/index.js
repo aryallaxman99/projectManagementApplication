5c3bdbb24ffc6d4383d3e6e317defe198efe6fc1
aryallaxman99 <aryallaxman99@gmail.com>
2023-01-31 15:06:37 +0545

    removed unwanted files

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,22 @@
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
 import connect from "./db/connect.js";
 import userRouter from "./routes/userRouter.js";
-const port = process.env.PORT;
 
 const app = express();
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("home");
-});
-
 app.use("/user", userRouter);
 
-app.listen(5000, (err) => {
+app.listen(process.env.PORT, (err) => {
   if (!err) {
-    console.log(`server started at ${port}`);
+    console.log(`server started at ${process.env.PORT}`);
     connect();
   } else {
     console.error(err);
